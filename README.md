fc437e8751d2c9441445d3e8011b81aa7214eb70
MaryAnzh <maryang@mail.ru>
2022-10-15 12:27:44 +0300

    init: create vue-3 cli app

---
+++ README.md
@@ -0,0 +1,7 @@
+# Vue-3-Test
+
+My first app on Vue-3
+
+training links
+1. https://www.youtube.com/watch?v=xJYBDNWwlX4
+2. https://ru.vuejs.org
---
+++ src/App.vue
@@ -0,0 +1,14 @@
+<template>
+  <header>
+    <nav>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </nav>
+  </header>
+  <main>
+    <router-view />
+  </main>
+  <footer>
+    <a href="https://github.com/MaryAnzh" target="_blank">©MaryAnzh, 2022</a>
+  </footer>
+</template>
