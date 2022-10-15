90e3154ae30c5f1cd07da5c941eae2441613cac4
MaryAnzh <maryang@mail.ru>
2022-10-15 22:28:46 +0300

    feat: add style

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
+  <h2>Home page</h2>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <!-- <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
   </div>
 </template>
 
